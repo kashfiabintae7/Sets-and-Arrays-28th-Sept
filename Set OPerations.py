5b80e38b09800ba1dd066b2cd08633e90f490aa9
Kashfia <kashfiabintae@gmail.com>
2024-09-28 16:55:22 +0600

    first commit

---
+++ Set OPerations.py
@@ -0,0 +1,16 @@
+mySet = {1, 2, 3, 3, 4, 5, 5, 5}
+print(mySet)
+
+mySet.add("Junaira")
+mySet.add("Kashfia")
+mySet.add("Tabassum")
+print(mySet)
+
+mySet.pop()
+mySet.pop()
+print(mySet)
+
+
+l1 = [1, 2, 3, 2]
+setList = set(l1)
+print(setList)
